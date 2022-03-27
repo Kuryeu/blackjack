e31496843dc171c5c2fe547be0a80f69ec8e48fa
Brice <83395100+Brice9957@users.noreply.github.com>
2022-03-27 10:31:23 -0400

    Classe Jeu de Carte

---
+++ BlackJack/JeuDeCarte.py
@@ -0,0 +1,32 @@
+import random
+
+class JeuDeCarte:
+
+    def __init__(self, cartes):
+        self.cartes = cartes
+
+    def create_cartes_set (self):
+        self.cartes = [['2 de coeur', 2], ['2 de pique', 2], ['2 de carreau', 2], ['2 de trefle', 2],
+                  ['3 de coeur', 3], ['3 de pique', 3], ['3 de carreau', 3], ['3 de trefle', 3],
+                  ['4 de coeur', 4], ['4 de pique', 4], ['4 de carreau', 4], ['4 de trefle', 4],
+                  ['5 de coeur', 5], ['5 de pique', 5], ['5 de carreau', 5], ['5 de trefle', 5],
+                  ['6 de coeur', 6], ['6 de pique', 6], ['6 de carreau', 6], ['6 de trefle', 6],
+                  ['7 de coeur', 7], ['7 de pique', 7], ['7 de carreau', 7], ['7 de trefle', 7],
+                  ['8 de coeur', 8], ['8 de pique', 8], ['8 de carreau', 8], ['8 de trefle', 8],
+                  ['9 de coeur', 2], ['9 de pique', 9], ['9 de carreau', 9], ['9 de trefle', 9],
+                  ['10 de coeur', 10], ['10 de pique', 10], ['10 de carreau', 10], ['10 de trefle', 10],
+                  ['valet de coeur', 10], ['valet de pique', 10], ['valet de carreau', 10], ['valet de trefle', 10],
+                  ['dame de coeur', 10], ['dame de pique', 10], ['dame de carreau', 10], ['dame de trefle', 10],
+                  ['roi de coeur', 10], ['roi de pique', 10], ['roi de carreau', 10], ['roi de trefle', 10],
+                  ['as de coeur', [0,10]], ['as de pique', [0,10]], ['as de carreau', [0,10]], ['as de trefle', [0,10]]]
+
+    def tirer_carte(self):
+        return self.cartes[-1]
+
+    def melanger_cartes(self):
+        random.shuffle(self.cartes)
+        print("le jeu a été mélangé : "+str(self.cartes))
+
+    def bruler_carte(self):
+        self.cartes.remove(self.cartes[0])
+        print("Une carte a été brûlée")
---
+++ BlackJack/main.py
@@ -0,0 +1,98 @@
+# This is a sample Python script.
+
+# Press Maj+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+import random
+
+from JeuDeCarte import JeuDeCarte
+
+
+def coups_possible(main_joueur, historique_coups, main_croupier, nombre_main_joueur):
+    liste_coups_possible = ["Arreter"]
+    is_as = False
+    nombre_as = 0
+    for carte in main_joueur:
+        if carte[0][:2] == 'as':
+            nombre_as += 1
+            is_as = True
+
+    ##### Doubler la mise
+
+    if is_as:
+        s1 = 0
+        s2 = 0
+        for carte in main_joueur:
+            if carte[0][:2] == 'as':
+                s1 += 1
+                s2 += 10
+            else:
+                s1 += carte[1]
+                s2 += carte[1]
+        if s1 != 21 and s2 != 21:
+            if "Partager_pair_as" not in historique_coups:
+                liste_coups_possible.append("Doubler")
+    else:
+        s = 0
+        for carte in main_joueur:
+            s += carte[1]
+        if s != 21:
+            if "Partager_pair_as" not in historique_coups:
+                liste_coups_possible.append("Doubler")
+
+    ##### Partager
+
+    if is_as:
+        if nombre_as == 2:
+            if "Partage_pair_as" not in historique_coups and nombre_main_joueur < 4:
+                liste_coups_possible.append("Partager_pair_as")
+        else:
+            s = 0
+            for carte in main_joueur:
+                if carte[0][:2] == 'as':
+                    s += 1
+                else:
+                    s += carte[1]
+            if s != 21 and "Partage_pair_as" not in historique_coups and nombre_main_joueur < 4:
+                liste_coups_possible.append("Partager")
+    else:
+        if main_joueur[0][1] == main_joueur[1][1]:
+            if "Partage_pair_as" not in historique_coups and nombre_main_joueur < 4:
+                liste_coups_possible.append("Partager")
+
+    ##### Assurer
+
+    croupier_a_as = False
+    for carte in main_croupier:
+        if carte[0][:2] == 'as':
+            croupier_a_as = True
+
+    if croupier_a_as:
+        liste_coups_possible.append("Assurer")
+
+    ##### Tirer
+
+    if "Partage_pair_as" not in historique_coups:
+        if "Arreter" not in historique_coups:
+            liste_coups_possible.append("Tirer_plusieurs_cartes")
+    else:
+        if "Arreter" not in historique_coups:
+            liste_coups_possible.append("Tirer_une_carte")
+
+    return liste_coups_possible
+
+
+
+
+
+
+cartes = []
+jeuDeCarte1 = JeuDeCarte(cartes)
+jeuDeCarte1.create_cartes_set()
+
+main_joueur = [['as de pique', [1,10]], ['10 de carreau', 10]]
+historique_coup = []
+main_croupier = [['roi de pique', 10], ['6 de carreau', 6]]
+
+print(coups_possible(main_joueur, historique_coup, main_croupier, 1))
+
